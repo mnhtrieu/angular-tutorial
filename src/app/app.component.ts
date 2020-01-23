a58e0c4c729632551392ff50b213680348b54317
Minh Trieu <mnhtrieu@gmail.com>
2020-01-23 10:21:02 +0100

    angular universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,19 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private authService: AuthService, private loggingService: LoggingService, private store: Store<fromApp.AppState>) { }
+  constructor(
+    private authService: AuthService, 
+    private loggingService: LoggingService, 
+    private store: Store<fromApp.AppState>, 
+    @Inject(PLATFORM_ID) private platformId // detect the server or client 
+    ) 
+    { }
 
   ngOnInit(){
     // this.authService.autoLogin();
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if(isPlatformBrowser(this.platformId)){
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
 }
